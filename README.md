fd2bb15c3eef3e906b258c9f65d7a701bc6be692
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-12-07 18:30:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_hand_sign_alphabet_lower_case
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_hand_sign_alphabet_lower_case/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_hand_sign_alphabet_lower_case
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
